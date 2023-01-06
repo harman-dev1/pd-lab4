140fc2cc8166f69734e33b5a87a205fe38ee0196
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-06 13:41:05 +0500

    Add files via upload

---
+++ task5.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+void A();
+void W();
+void A();
+void I();
+void S();
+main()
+{
+  A();
+  W();
+  A();
+  I();
+  S();
+}
+void A()
+{
+  cout<<"       A           "<<endl;
+  cout<<"     A   A         "<<endl;
+  cout<<"    A     A        "<<endl;
+  cout<<"   AAAAAAAAA       "<<endl;
+  cout<<"   A       A       "<<endl;
+  cout<<"   A       A       "<<endl;
+  cout<<"   A       A       "<<endl;
+}
+void W()
+{
+  cout<<"W       W      W   "<<endl;
+  cout<<"W    W    W    W   "<<endl;
+  cout<<"W   W      W   W   "<<endl;
+  cout<<"W  W        W  W   "<<endl;
+  cout<<"W W          W W    "<<endl;
+}
+void I()     
+{
+  cout<<"    IIIIIII  "<<endl;
+  cout<<"      III    "<<endl;
+  cout<<"      III    "<<endl;
+  cout<<"      III    "<<endl;
+  cout<<"      III    "<<endl;
+  cout<<"      III    "<<endl;
+  cout<<"    IIIIIII  "<<endl;
+}
+void S()
+{
+  cout<<"     S S S       "<<endl;
+  cout<<"    S            "<<endl;
+  cout<<"   S             "<<endl;
+  cout<<"     S           "<<endl;
+  cout<<"       S        "<<endl;
+  cout<<"         S       "<<endl;
+  cout<<"         S        "<<endl;
+  cout<<"   S S S         "<<endl;
+}
